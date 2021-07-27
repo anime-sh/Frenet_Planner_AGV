875eead1b776f8ddd41a02966605a3a10be0dc22
thefurorjuror <ranjanmail.rohit@gmail.com>
2021-07-27 17:42:54 +0530

    optimised global frame conversion, collision check and multiprocessing added
    draft.py deleted

--- frenet_planner/frenet_planner/frenet_helpers.py
+++ frenet_planner/frenet_planner/frenet_helpers.py
@@ -1,10 +1,12 @@
-
+#!/usr/bin/env python
 from frenet_planner import params
 import numpy as np
 import copy
 import math
 import bisect
 import time
+import multiprocessing as mp
+from joblib import Parallel, delayed
 
 # import messages
 from nav_msgs.msg import Path
@@ -42,112 +44,108 @@ class FrenetPath:
         self.ds = []
         self.c = []
 
-# calculate list of frenet paths given start and end states
+# # calculate list of frenet paths given start and end states
 def calc_frenet_paths(c_speed, c_d, c_d_d, c_d_dd, s0):
     frenet_paths = []
-    if (params.STOP_CAR):
-        for di in np.arange(-params.MAX_ROAD_WIDTH, params.MAX_ROAD_WIDTH, params.D_ROAD_W):
-            for Ti in np.arange(params.DT, params.MAX_T, params.DT):
-                fp = FrenetPath()
-                lat_qp = QuinticPolynomial(c_d, c_d_d, c_d_dd, di, 0.0, 0.0, Ti)
-                fp.t = [t for t in np.arange(0.0, Ti, params.DT)]
-                fp.d = [lat_qp.calc_point(t) for t in fp.t]
-                fp.d_d = [lat_qp.calc_first_derivative(t) for t in fp.t]
-                fp.d_dd = [lat_qp.calc_second_derivative(t) for t in fp.t]
-                fp.d_ddd = [lat_qp.calc_third_derivative(t) for t in fp.t]
-
-                tfp = copy.deepcopy(fp)
-                lon_qp = QuinticPolynomial(s0, c_speed, 0.0, min(s0+params.LOOKAHEAD_DIST,params.s_dest),params.TARGET_SPEED, 0.0, Ti)
-                tfp.s = [lon_qp.calc_point(t) for t in fp.t]
-                tfp.s_d = [lon_qp.calc_first_derivative(t) for t in fp.t]
-                tfp.s_dd = [lon_qp.calc_second_derivative(t) for t in fp.t]
-                tfp.s_ddd = [lon_qp.calc_third_derivative(t) for t in fp.t]
-
-                Jp = sum(np.power(tfp.d_ddd, 2))  # square of jerk
-                Js = sum(np.power(tfp.s_ddd, 2))  # square of jerk
-
-                # square of diff from target speed
-                ds = (params.TARGET_SPEED - tfp.s_d[-1]) ** 2
-
-                tfp.cd = params.K_J * Jp + params.K_T * Ti + params.K_D * tfp.d[-1] ** 2
-                tfp.cv = params.K_J * Js + params.K_T * Ti + params.K_D * ds
-                tfp.cf = params.K_LAT * tfp.cd + params.K_LON * tfp.cv
-
-                frenet_paths.append(tfp)
+    if (params.STOP_CAR): 
+        frenet_paths=Parallel(n_jobs=1)(delayed(calc_frenet_path_stop)(di,Ti,c_speed, c_d, c_d_d, c_d_dd, s0) for di in np.arange(-params.MAX_ROAD_WIDTH, params.MAX_ROAD_WIDTH, params.D_ROAD_W) for Ti in np.arange(params.MIN_T, params.MAX_T, params.DT) )
         return frenet_paths
 
-
     # generate path to each offset goal
-    for di in np.arange(-params.MAX_ROAD_WIDTH, params.MAX_ROAD_WIDTH, params.D_ROAD_W):
-        # Lateral motion planning
-        for Ti in np.arange(params.MIN_T, params.MAX_T, params.DT):
-            for Di_d in np.arange(-params.MAX_LAT_VEL,params.MAX_LAT_VEL+0.001,params.D_D_NS):
-                fp = FrenetPath()
-
-                lat_qp = QuinticPolynomial(c_d, c_d_d, c_d_dd, di, Di_d, 0.0, Ti)
-
-                fp.t = [t for t in np.arange(0.0, Ti, params.DT)]
-                fp.d = [lat_qp.calc_point(t) for t in fp.t]
-                fp.d_d = [lat_qp.calc_first_derivative(t) for t in fp.t]
-                fp.d_dd = [lat_qp.calc_second_derivative(t) for t in fp.t]
-                fp.d_ddd = [lat_qp.calc_third_derivative(t) for t in fp.t]
-
-                # Longitudinal motion planning (Velocity keeping)
-                for tv in np.arange(params.TARGET_SPEED - params.D_T_S * params.N_S_SAMPLE,
-                                    params.TARGET_SPEED + params.D_T_S * params.N_S_SAMPLE, params.D_T_S):
-                    tfp = copy.deepcopy(fp)
-                    lon_qp = QuinticPolynomial(s0, c_speed, 0.0, s0+params.LOOKAHEAD_DIST,tv, 0.0, Ti)
-
-                    tfp.s = [lon_qp.calc_point(t) for t in fp.t]
-                    tfp.s_d = [lon_qp.calc_first_derivative(t) for t in fp.t]
-                    tfp.s_dd = [lon_qp.calc_second_derivative(t) for t in fp.t]
-                    tfp.s_ddd = [lon_qp.calc_third_derivative(t) for t in fp.t]
-
-                    Jp = sum(np.power(tfp.d_ddd, 2))  # square of jerk
-                    Js = sum(np.power(tfp.s_ddd, 2))  # square of jerk
-
-                    # square of diff from target speed
-                    ds = (params.TARGET_SPEED - tfp.s_d[-1]) ** 2
-
-                    tfp.cd = params.K_J * Jp + params.K_T * Ti + params.K_D * tfp.d[-1] ** 2
-                    tfp.cv = params.K_J * Js + params.K_T * Ti + params.K_D * ds
-                    tfp.cf = params.K_LAT * tfp.cd + params.K_LON * tfp.cv
-
-                    frenet_paths.append(tfp)
+    frenet_paths=Parallel(n_jobs=mp.cpu_count())(delayed(calc_frenet_path_non_stop)(di,Ti,Di_d,tv,c_speed, c_d, c_d_d, c_d_dd, s0) for di in np.arange(-params.MAX_ROAD_WIDTH, params.MAX_ROAD_WIDTH, params.D_ROAD_W) for Ti in np.arange(params.MIN_T, params.MAX_T, params.DT) for Di_d in np.arange(-params.MAX_LAT_VEL,params.MAX_LAT_VEL+0.001,params.D_D_NS) for tv in np.arange(params.TARGET_SPEED - params.D_T_S * params.N_S_SAMPLE,params.TARGET_SPEED + params.D_T_S * params.N_S_SAMPLE, params.D_T_S)) 
     return frenet_paths
 
+def calc_frenet_path_stop(di,Ti,c_speed, c_d, c_d_d, c_d_dd, s0):
+    fp = FrenetPath()
+    lat_qp = QuinticPolynomial(c_d, c_d_d, c_d_dd, di, 0.0, 0.0, Ti)
+    fp.t = [t for t in np.arange(0.0, Ti, params.DT)]
+    fp.d = [lat_qp.calc_point(t) for t in fp.t]
+    fp.d_d = [lat_qp.calc_first_derivative(t) for t in fp.t]
+    fp.d_dd = [lat_qp.calc_second_derivative(t) for t in fp.t]
+    fp.d_ddd = [lat_qp.calc_third_derivative(t) for t in fp.t]
+
+    tfp = copy.deepcopy(fp)
+    lon_qp = QuinticPolynomial(s0, c_speed, 0.0, min(s0+params.LOOKAHEAD_DIST,params.s_dest),params.TARGET_SPEED, 0.0, Ti)
+    tfp.s = [lon_qp.calc_point(t) for t in fp.t]
+    tfp.s_d = [lon_qp.calc_first_derivative(t) for t in fp.t]
+    tfp.s_dd = [lon_qp.calc_second_derivative(t) for t in fp.t]
+    tfp.s_ddd = [lon_qp.calc_third_derivative(t) for t in fp.t]
+
+    Jp = sum(np.power(tfp.d_ddd, 2))  # square of jerk
+    Js = sum(np.power(tfp.s_ddd, 2))  # square of jerk
+
+    # square of diff from target speed
+    ds = (params.TARGET_SPEED - tfp.s_d[-1]) ** 2
+
+    tfp.cd = params.K_J * Jp + params.K_T * Ti + params.K_D * tfp.d[-1] ** 2
+    tfp.cv = params.K_J * Js + params.K_T * Ti + params.K_D * ds
+    tfp.cf = params.K_LAT * tfp.cd + params.K_LON * tfp.cv
+    # print(tfp.s)
+    return tfp
+
+def calc_frenet_path_non_stop(di,Ti,Di_d,tv,c_speed, c_d, c_d_d, c_d_dd, s0):
+    fp = FrenetPath()
+
+    lat_qp = QuinticPolynomial(c_d, c_d_d, c_d_dd, di, Di_d, 0.0, Ti)
+
+    fp.t = [t for t in np.arange(0.0, Ti, params.DT)]
+    fp.d = [lat_qp.calc_point(t) for t in fp.t]
+    fp.d_d = [lat_qp.calc_first_derivative(t) for t in fp.t]
+    fp.d_dd = [lat_qp.calc_second_derivative(t) for t in fp.t]
+    fp.d_ddd = [lat_qp.calc_third_derivative(t) for t in fp.t]
+
+    tfp = copy.deepcopy(fp)
+    lon_qp = QuinticPolynomial(s0, c_speed, 0.0, s0+params.LOOKAHEAD_DIST,tv, 0.0, Ti)
+
+    tfp.s = [lon_qp.calc_point(t) for t in fp.t]
+    tfp.s_d = [lon_qp.calc_first_derivative(t) for t in fp.t]
+    tfp.s_dd = [lon_qp.calc_second_derivative(t) for t in fp.t]
+    tfp.s_ddd = [lon_qp.calc_third_derivative(t) for t in fp.t]
+
+    Jp = sum(np.power(tfp.d_ddd, 2))  # square of jerk
+    Js = sum(np.power(tfp.s_ddd, 2))  # square of jerk
+
+    # square of diff from target speed
+    ds = (params.TARGET_SPEED - tfp.s_d[-1]) ** 2
+
+    tfp.cd = params.K_J * Jp + params.K_T * Ti + params.K_D * tfp.d[-1] ** 2
+    tfp.cv = params.K_J * Js + params.K_T * Ti + params.K_D * ds
+    tfp.cf = params.K_LAT * tfp.cd + params.K_LON * tfp.cv
+
+    return tfp
+
+
 # convert frenet paths to global frame
-def calc_global_paths(fplist, csp):
-    for fp in fplist:
+def calc_global_paths(fp, csp):
 
-        # calc global positions
-        for i in range(len(fp.s)):
-            ix, iy = csp.calc_position(fp.s[i])
-            if ix is None:
-                break
-            i_yaw = csp.calc_yaw(fp.s[i])
-            di = fp.d[i]
-            fx = ix + di * math.cos(i_yaw + math.pi / 2.0)
-            fy = iy + di * math.sin(i_yaw + math.pi / 2.0)
-            fp.x.append(fx)
-            fp.y.append(fy)
+    # calc global positions
+    for i in range(len(fp.s)):
+        ix, iy = csp.calc_position(fp.s[i])
+        if ix is None:
+            break
+        i_yaw = csp.calc_yaw(fp.s[i])
+        di = fp.d[i]
+        fx = ix + di * math.cos(i_yaw + math.pi / 2.0)
+        fy = iy + di * math.sin(i_yaw + math.pi / 2.0)
+        fp.x.append(fx)
+        fp.y.append(fy)
 
-        # calc yaw and ds
-        if(len(fp.x)>1):
-            for i in range(len(fp.x) - 1):
-                dx = fp.x[i + 1] - fp.x[i]
-                dy = fp.y[i + 1] - fp.y[i]
-                fp.yaw.append(math.atan2(dy, dx))
-                fp.ds.append(math.hypot(dx, dy))
+    # calc yaw and ds
+    if(len(fp.x)>1):
+        for i in range(len(fp.x) - 1):
+            dx = fp.x[i + 1] - fp.x[i]
+            dy = fp.y[i + 1] - fp.y[i]
+            fp.yaw.append(math.atan2(dy, dx))
+            fp.ds.append(math.hypot(dx, dy))
 
-            fp.yaw.append(fp.yaw[-1])
-            fp.ds.append(fp.ds[-1])
+        fp.yaw.append(fp.yaw[-1])
+        fp.ds.append(fp.ds[-1])
 
-        # calc curvature
-        for i in range(len(fp.yaw) - 1):
-            fp.c.append((fp.yaw[i + 1] - fp.yaw[i]) / fp.ds[i])
+    # calc curvature
+    for i in range(len(fp.yaw) - 1):
+        fp.c.append((fp.yaw[i + 1] - fp.yaw[i]) / fp.ds[i])
 
-    return fplist
+    return fp
 
 # calculate footprint polygon at given position on path via transformation of bot footprint 
 # simple rotation and translation via math referenced here -> https://drive.google.com/file/d/1_xbz9cf8KGwfHLOJU2muVP7nrP_SInJE/view?usp=sharing
@@ -194,21 +192,13 @@ def nearest_obs(point32):
 
     if it==0:
         if(calc_dis(p_x,p_y,ob_x[it],ob_y[it])<= params.OBSTACLE_RADIUS):
-            # ob_x.pop(it)
-            # ob_y.pop(it)
             return False
     elif it==len(ob_x):
         it = it-1
         if(calc_dis(p_x,p_y,ob_x[it],ob_y[it])<= params.OBSTACLE_RADIUS):
-            # ob_x.pop(it)
-            # ob_y.pop(it)
             return False
     else:
         if(calc_dis(p_x,p_y,ob_x[it],ob_y[it])<= params.OBSTACLE_RADIUS) and (calc_dis(p_x,p_y,ob_x[it-1],ob_y[it-1])> params.OBSTACLE_RADIUS):
-            # ob_x.pop(it)
-            # ob_y.pop(it)
-            # ob_x.pop(it-1)
-            # ob_y.pop(it-1)
             return False
 
     it = bisect.bisect_left(ob_y,p_y,lo = 0,hi = len(ob_y))
@@ -225,26 +215,6 @@ def nearest_obs(point32):
             return False
 
     return True
-
-# # check for obstacles in a radius given by OBSTACLE_RADIUS around each vertice of transformed polygon
-# def check_collision(fp):
-#     if(params.ob==[]):
-#         return True
-#     for i in range(min(len(fp.x),len(fp.yaw))):
-#         trans_footprint = transformation(params.footprint.polygon.points,params.odom.pose.pose,fp.x[i],fp.y[i],fp.yaw[i])
-#         for j in trans_footprint:
-#                 # for i in range(params.ob.shape[0]): #currently checks with every obstacle 
-#                 #     ix = j.x
-#                 #     iy = j.y
-#                 #     d = ((ix - params.ob[i, 0]) ** 2) + ((iy - params.ob[i, 1]) ** 2)
-#                 #     collision = (d <= params.OBSTACLE_RADIUS ** 2 )
-#                 #     if collision:
-#                 #         return False
-#                 collision = nearest_obs(j)
-#                 if not collision:
-#                     return False
-
-#     return True
 
 # faster check for obstacles in a radius given by OBSTACLE_RADIUS around 3 bounding circle centres
 def check_collision(fp):
@@ -267,13 +237,6 @@ def check_collision(fp):
     for i in range(min(len(fp.x),len(fp.yaw))):
         trans_footprint = transformation(params.footprint.polygon.points,params.odom.pose.pose,fp.x[i],fp.y[i],fp.yaw[i])
         for j in trans_footprint:
-                # for i in range(params.ob.shape[0]): #currently checks with every obstacle 
-                #     ix = j.x
-                #     iy = j.y
-                #     d = ((ix - params.ob[i, 0]) ** 2) + ((iy - params.ob[i, 1]) ** 2)
-                #     collision = (d <= params.OBSTACLE_RADIUS ** 2 )
-                #     if collision:
-                #         return False
                 collision = nearest_obs(j)
                 if not collision:
                     return False
@@ -281,36 +244,18 @@ def check_collision(fp):
     return True
 
 # run collision checks and constraint-checking on all initially generated paths
-def check_paths(fplist):
-    ok_ind = []
-    # speed=0
-    # acc=0
-    # curvature=0
-    # coll=0
+def check_paths(fp):
+    if any([v > params.MAX_SPEED for v in fp.s_d]):  # Max speed check
+        return False
+    elif any([abs(a) > params.MAX_ACCEL for a in
+                fp.s_dd]):  # Max accel check
+        return False
+    elif any([abs(c) > params.MAX_CURVATURE for c in
+                fp.c]):  # Max curvature check
+        return False
 
-    for i, _ in enumerate(fplist):
-        if any([v > params.MAX_SPEED for v in fplist[i].s_d]):  # Max speed check
-            # speed+=1
-            continue
-        elif any([abs(a) > params.MAX_ACCEL for a in
-                  fplist[i].s_dd]):  # Max accel check
-            # acc+=1
-            continue
-        elif any([abs(c) > params.MAX_CURVATURE for c in
-                  fplist[i].c]):  # Max curvature check
-            # curvature+=1
-            continue
-        elif not check_collision(fplist[i]):
-            # coll+=1
-            continue
+    return check_collision(fp)
 
-        ok_ind.append(i)
-    # print("s="+str(speed))
-    # print("a="+str(acc))
-    # print("c="+str(curvature))
-    # print("coll="+str(coll))
-    # print("remaining="+str(len(ok_ind)))
-    return [fplist[i] for i in ok_ind]
 
 # generate central spline from given waypoint coordinates
 def generate_target_course(x, y):
@@ -333,26 +278,22 @@ def frenet_optimal_planning(csp, s0, c_speed, c_d, c_d_d, c_d_dd):
     fplist = calc_frenet_paths(c_speed, c_d, c_d_d, c_d_dd, s0)
     end_calc_frenet=time.time()
     print("calc_frenet_paths time elapsed: "+str(end_calc_frenet -start_calc_frenet))
-    start_calc_global=time.time()
-    fplist = calc_global_paths(fplist, csp)
-    end_calc_global=time.time()
-    print("calc_global_paths time elapsed: "+str(end_calc_global -start_calc_global))
-    start_check_path=time.time()
-    fplist = check_paths(fplist)
-    end_check_path=time.time()
-    print("check_path time elapsed: "+str(end_check_path -start_check_path))
 
+    try: import operator
+    except ImportError: keyfun= lambda x: x.cf # use a lambda if no operator module
+    else: keyfun= operator.attrgetter("cf") # use operator since it's faster than lambda
 
-    # find minimum cost path
-    min_cost = params.FLT_MAX
-    best_path = None
+    fplist.sort(key=keyfun, reverse=False) # sort in-place
+
     for fp in fplist:
-        if min_cost >= fp.cf:
-            min_cost = fp.cf
-            best_path = fp
-
-    return best_path
-
+        start_calc_global=time.time()
+        fp = calc_global_paths(fp, csp)
+        if check_paths(fp):
+            end_calc_global=time.time()
+            print("calc_global_paths time elapsed: "+str(end_calc_global -start_calc_global))
+            return fp
+    else:
+        print("No path!")
 
 # calculate target velocity
 def calc_bot_v( d, s_d, d_d,rk):
